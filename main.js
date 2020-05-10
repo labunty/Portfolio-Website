c5692300642de7681b2233d115374afc9410a33a
lauren bunting <laurenbunting@Laurens-MacBook-Air.local>
2020-05-10 18:41:15 -0500

    changes

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
+var btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
 for(let i = 1; i <= 5; i++) {
-  const newImage = document.createElement('img');
-  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/blackbear album cover original.jpg' + i + '.jpg');
+  newImage.setAttribute('src', 'images/the avett brothers album cover.jpg' + i + '.jpg');
+  newImage.setAtribute('src', 'images/rex orange county front album cover.jpg' + i + '.jpg');
+  newImage.setAttribute('src', 'images/the lumineers album cover.jpg' + i + '.jpg');
   thumbBar.appendChild(newImage);
-  newImage.onclick = function(e) {
-    displayedImage.src = e.target.src;
   }
 }
 
 btn.onclick = function() {
-  const btnClass = btn.getAttribute('class');
+  var btnClass = btn.getAttribute('class');
   if(btnClass === 'dark') {
-    btn.setAttribute('class','light');
+    btn.setAttribute('class', 'light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
+    overlay.style.backgroundColor = 'rgba(197, 84, 34, 0.53)';
+  }
+  else {
     btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    overlay.style.backgroundColor = 'rgba(197, 84, 34, 0.38)';
   }
 }
