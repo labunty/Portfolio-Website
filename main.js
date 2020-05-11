cbaa444e28aefc0da25359269ffccaf67e1ebfd6
lauren bunting <laurenbunting@Laurens-MacBook-Air.local>
2020-05-11 15:57:02 -0500

    changes

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+  const btn = document.querySelector('button');
+    const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
 
 for(let i = 1; i <= 5; i++) {
   const newImage = document.createElement('img');
@@ -15,15 +14,14 @@ for(let i = 1; i <= 5; i++) {
   }
 }
 
-/* Wiring up the Darken/Lighten button */
 
 btn.onclick = function() {
-  const btnClass = btn.getAttribute('class');
+    const btnClass = btn.getAttribute('class');
   if(btnClass === 'dark') {
     btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
+        }  else {
     btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
